c4cb94d2dd5e8afbd878bd95f9089c1ac71785b2
Izak Villanueva <izakvillanueva14@gmail.com>
2024-05-04 20:55:41 +0800

    Added register and login page

--- client/src/App.js
+++ client/src/App.js
@@ -29,7 +29,7 @@ function App() {
             />
             <Route 
               path="/login" 
-              element={isAuth ? <LoginPage /> : <Navigate to="/home" />} 
+              element={<LoginPage />} 
             />
             <Route
               path="/profile/:userId"
