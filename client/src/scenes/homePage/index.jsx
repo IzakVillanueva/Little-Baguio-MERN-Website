baee4d9733645c9eadc0d236f0f1bf08c190491c
Izak Villanueva <izakvillanueva14@gmail.com>
2024-05-05 02:14:25 +0800

    Update home

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -1,10 +1,19 @@
 import { Box, useMediaQuery } from "@mui/material";
 import { useSelector } from "react-redux";
 import Navbar from "scenes/navbar";
+import './home.css';
 
 const HomePage = () => {
     return (<Box>
         <Navbar />
+        <section class="hero">
+            <div class="container">
+                <div class="hero-text">
+                    <h1>Be a part of the Community!</h1>
+                    <p>Get the latest updates, connect with others, and benefit from the services we offer.</p>
+                </div>
+            </div>
+        </section>
     </Box>);
 };
 
