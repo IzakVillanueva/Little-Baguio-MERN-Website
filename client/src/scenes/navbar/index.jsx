ebabb9d50a402c8ee8a07784de89ec6ba4b5a9f4
Izak Villanueva <izakvillanueva14@gmail.com>
2024-05-05 17:09:58 +0800

    Fix navbar no user

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -39,8 +39,15 @@ const Navbar = () => {
   const background = theme.palette.background.default;
   const primaryLight = theme.palette.primary.light;
   const alt = theme.palette.background.alt;
+  let isLoggedOut = true;
+  let username = '';
 
-  const username = `${user.username}`; // ${user.username}
+  if (user !== null && user !== undefined) {
+    username = `${user.username}`; // Assign user.username if it's not null or undefined
+    isLoggedOut = false
+  } else {
+    isLoggedOut = true;
+  }
 
   return (
     <FlexBetween padding="1rem 6% 1rem 2%" backgroundColor={alt}>
@@ -91,30 +98,39 @@ const Navbar = () => {
           <Message sx={{ fontSize: "25px" }} />
           <Notifications sx={{ fontSize: "25px" }} />
           <Help sx={{ fontSize: "25px" }} />
-          <FormControl variant="standard" value={username}>
-            <Select
-              value={username}
-              sx={{
-                backgroundColor: neutralLight,
-                width: "150px",
-                borderRadius: "0.25rem",
-                p: "0.25rem 1rem",
-                "& .MuiSvgIcon-root": {
-                  pr: "0.25rem",
-                  width: "3rem",
-                },
-                "& .MuiSelect-select:focus": {
+          {isLoggedOut ? (
+            <div style={{ height: '50%' }}>
+              <button className="loginBut" onClick={() => navigate("/login")} style={{cursor: 'pointer'}}>
+                  <h3>Login</h3>
+              </button>
+            </div>
+          ) : (
+            <FormControl variant="standard" value={username}> {/* DROPDOWN SHOW USER */}
+              <Select
+                value={username}
+                sx={{
                   backgroundColor: neutralLight,
-                },
-              }}
-              input={<InputBase />}
-            >
-              <MenuItem value={username}>
-                <Typography>{username}</Typography>
-              </MenuItem>
-              <MenuItem onClick={() => dispatch(setLogout())}>Log Out</MenuItem>
-            </Select>
-          </FormControl>
+                  width: "150px",
+                  borderRadius: "0.25rem",
+                  p: "0.25rem 1rem",
+                  "& .MuiSvgIcon-root": {
+                    pr: "0.25rem",
+                    width: "3rem",
+                  },
+                  "& .MuiSelect-select:focus": {
+                    backgroundColor: neutralLight,
+                  },
+                }}
+                input={<InputBase />}
+              >
+                <MenuItem value={username}>
+                  <Typography>{username}</Typography>
+                </MenuItem>
+                <MenuItem onClick={() => dispatch(setLogout())}>Log Out</MenuItem>
+              </Select>
+            </FormControl>
+          )}
+          
         </FlexBetween>
       ) : (
         <IconButton
