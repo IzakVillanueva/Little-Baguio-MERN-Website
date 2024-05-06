fd949e2abf93acb50dd5415ae4866c651ffe0837
Izak Villanueva <izakvillanueva14@gmail.com>
2024-05-06 20:52:29 +0800

    Fix cors

--- server/index.js
+++ server/index.js
@@ -22,6 +22,7 @@ import { users, posts } from "./data/index.js";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 dotenv.config();
+const cors = require('cors');
 const app = express();
 app.use(cors(
     {
@@ -36,7 +37,6 @@ app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin"}));
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, 'public/assets')));
 
 /* FILE STORAGE */
