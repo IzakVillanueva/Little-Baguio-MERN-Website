9b85f5ff4ebafa1719c45a5994fe7ec3c7ee9917
Izak Villanueva <izakvillanueva14@gmail.com>
2024-05-06 20:59:03 +0800

    Update express

--- server/index.js
+++ server/index.js
@@ -22,6 +22,7 @@ import { users, posts } from "./data/index.js";
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const app = express();
 app.use(cors(
     {
         origin:["https://littlebaguio.vercel.app"],
@@ -32,7 +33,6 @@ app.use(cors(
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 dotenv.config();
-const app = express();
 app.use(express.json());
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin"}));
