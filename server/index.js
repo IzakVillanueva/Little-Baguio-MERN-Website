b81b8bbdf62770357890381553d429abb235cb3e
Izak Villanueva <izakvillanueva14@gmail.com>
2024-05-06 20:25:52 +0800

    Update index.js

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ dotenv.config();
 const app = express();
 app.use(cors(
     {
-        origin:[""],
+        origin:["https://littlebaguio-api.vercel.app"],
         methods: ["POST", "GET"],
         credentials: true
     }
