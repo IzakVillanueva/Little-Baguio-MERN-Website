de1babc250ef82db8305b460324538d807de07ef
Izak Villanueva <izakvillanueva14@gmail.com>
2024-05-08 21:33:19 +0800

    Add servicepage folder

---
+++ client/src/scenes/servicePage/index.jsx
@@ -0,0 +1,13 @@
+import { Box } from "@mui/material";
+import Navbar from "scenes/navbar";
+
+const ServicePage = () => {
+    return (
+        <Box>
+            <Navbar />
+            {/* ADD HERE */}
+        </Box>
+    );
+};
+
+export default ServicePage;
