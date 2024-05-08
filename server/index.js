b4469cea6f10d05eefb0141f3157b8c420a33f91
Izak Villanueva <izakvillanueva14@gmail.com>
2024-05-08 17:41:46 +0800

    Update cors add patch

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ import { users, posts } from "./data/index.js";
 const app = express();
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "https://littlebaguio.vercel.app");
-    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE, PATCH");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     next();
   });
