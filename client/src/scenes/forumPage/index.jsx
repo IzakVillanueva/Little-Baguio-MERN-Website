9e07d980f70faaf8c74c40f1653d0670b2680a34
Izak Villanueva <izakvillanueva14@gmail.com>
2024-05-09 22:24:44 +0800

    Create index.jsx

---
+++ client/src/scenes/forumPage/index.jsx
@@ -0,0 +1,13 @@
+import { Box } from "@mui/material";
+import Navbar from "scenes/navbar";
+
+const ForumPage = () => {
+    return (
+        <Box>
+            <Navbar />
+            {/* ADD HERE */}
+        </Box>
+    );
+};
+
+export default ForumPage;
