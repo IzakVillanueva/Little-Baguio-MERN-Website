ebe659344924316cde1e62bd55cad72d606bb782
Izak Villanueva <izakvillanueva14@gmail.com>
2024-05-10 03:58:11 +0800

    Update service

--- client/src/scenes/servicePage/index.jsx
+++ client/src/scenes/servicePage/index.jsx
@@ -1,11 +1,47 @@
-import { Box } from "@mui/material";
+import { Box, Button, TextField, Typography, useMediaQuery } from "@mui/material";
 import Navbar from "scenes/navbar";
+import { useState } from "react";
 
 const ServicePage = () => {
+    const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
+    const [value, setValue] = useState('');
+
+    const handleSubmit = (e) => {
+        setValue('');
+        e.preventDefault();
+    }
     return (
         <Box>
             <Navbar />
-            {/* ADD HERE */}
+            <Box
+                width="100%"
+                backgroundColor={"#A4DDDE"}
+                p="1rem 6%"
+                textAlign="center"
+            >
+                <Typography fontWeight="bold" fontSize="32px" color="#303030" padding="2rem">
+                    Services
+                </Typography>
+            </Box>
+            <Box backgroundColor={"#E2DFD5"} p={isNonMobileScreens ? "1.5em" : "0"}>
+                <Typography
+                        fontFamily={"Montserrat"}
+                        fontWeight={"700"}
+                        margin="auto"
+                        align="center"
+                        fontSize={isNonMobileScreens ? "3em" : "1em"}
+                    >
+                        Feedback & Complaints
+                </Typography>
+                <Box alignItems="center">
+                    <TextField value={value} 
+                        onChange={(e)=>setValue(e.target.value)}
+                        backgroundColor="#fff"/>
+                    <Button type="submit" onClick={handleSubmit} m="auto">
+                        Submit
+                    </Button>
+                </Box>
+            </Box>
         </Box>
     );
 };
