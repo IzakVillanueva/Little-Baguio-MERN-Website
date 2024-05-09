a598275731ffece44c601a5f97a50ac10b5819d8
Izak Villanueva <izakvillanueva14@gmail.com>
2024-05-09 22:48:40 +0800

    Update forumpage

--- client/src/scenes/forumPage/index.jsx
+++ client/src/scenes/forumPage/index.jsx
@@ -1,13 +1,43 @@
-import { Box } from "@mui/material";
+import { Box, Typography, useMediaQuery } from "@mui/material";
+import { useSelector } from "react-redux";
 import Navbar from "scenes/navbar";
+import MyForumWidget from "scenes/widgets/MyForumWidget";
 
 const ForumPage = () => {
-    return (
-        <Box>
-            <Navbar />
-            {/* ADD HERE */}
+    const user = useSelector((state) => state.user);
+    const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
+    const _id = user ? user._id : null;
+    const { picturePath } = user || {};
+
+    return (<Box>
+        <Navbar />
+        <Box
+            width="100%"
+            backgroundColor={"#A4DDDE"}
+            p="1rem 6%"
+            textAlign="center"
+        >
+            <Typography fontWeight="bold" fontSize="32px" color="#303030" padding="2rem">
+                News and Updates
+            </Typography>
         </Box>
-    );
+        <Box
+            justifyContent="center"
+            alignItems="center"
+            margin="auto"
+            display="flex"
+        >
+            <Box
+                width={isNonMobileScreens ? "60vw" : "100vw"}
+                maxWidth="800px"
+                margin="auto"
+            >
+                <MyForumWidget picturePath={picturePath} />
+                {/* <PostsWidget userId={_id} /> */}
+            </Box>
+            
+        </Box>
+    </Box>);
 };
 
 export default ForumPage;
